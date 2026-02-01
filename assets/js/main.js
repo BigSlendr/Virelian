c5ff74630e5f326927190a36ae74c33089152907
BigSlendr <michaelamarino16@gmail.com>
2026-02-01 20:39:28 +0000

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
+async function includePartials() {
+	const includeEls = document.querySelectorAll("[data-include]");
+	for (const el of includeEls) {
+		const file = el.getAttribute("data-include");
+		try {
+			const res = await fetch(file, { cache: "no-cache" });
+			if (!res.ok) throw new Error(`Failed to fetch ${file}`);
+			el.innerHTML = await res.text();
+		} catch (err) {
+			console.error(err);
+			el.innerHTML = "";
+		}
+	}
+}
+document.addEventListener("DOMContentLoaded", includePartials);
 
